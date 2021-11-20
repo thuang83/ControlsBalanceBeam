b231f2215bee85cf9306418ad144af3534ec9d8a
jlewis325 <jlewis325@gatech.edu>
2021-11-20 12:23:29 -0500

    Added circbuff.h and began cleaning up geometry calcs

---
+++ control/circbuff.h
@@ -0,0 +1,29 @@
+template <int n>
+class CircBuff {
+  private:
+    double buff[n];
+    int index = 0;
+    double sum;
+    bool buffer_full = false;
+
+  public:
+    CircBuff() {}
+
+    void addSample(double val) {
+      buff[index] = val;
+      sum += val;
+
+      buffer_full |= (index > 0 && (index + 1) % n == 0);
+      index = (index + 1) % n;
+      Serial.println(index);
+      sum -= buff[index];
+    }
+
+    double getAverage() {
+      if (buffer_full) {
+        return sum / n;
+      }
+
+      return NAN;
+    }
+};
--- control/control.ino
+++ control/control.ino
@@ -1,4 +1,5 @@
 #include <Servo.h>
+#include "circbuff.h"
 
 // calibration - I accidentally did this in inches
 #define A_ -0.02
@@ -14,12 +15,10 @@
 #define L_A1 0.162            // meters, distance from motor axis to center hinge axis
 #define L_B1 0.0279           // length of first motor linkage
 #define L_A2 0.161            // distance from center hinge axis to small hinge axis
-#define L_B2 0.0625           // length of second motor linkage
-#define D_H 0.0602            // height of beam obove motor when it is parallel to the ground
+#define L_B2 0.09             // length of second motor linkage
+#define D_H 0.09              // height of beam obove motor when it is parallel to the ground
 #define PHI_0 asin(D_H/L_A1)  // angle between beam and line connecting motor and center hinge axes when level
-                              // TODO: find this with calibration instead? Or use an accelerometer to sense it continuously?
 
-// code TODO: update angle limits based on new motor mounting disk and second linkage length
 #define LOOP_RATE_HZ 50
 #define MIN_MOTOR_ANGLE 45
 #define MAX_MOTOR_ANGLE 127
@@ -42,8 +41,13 @@ int sensor_pin = 14;
 int servo_pin = 15;
 double set_point = 0;
 double cur_pos = 0.0;
+double prev_pos = 0.0;
 double beam_angle = 0.0;
 double beam_angular_velocity = 0.0;
+
+CircBuff<LOOP_RATE_HZ * 5> circbuff;
+double beam_angle_offset = 0;
+bool beam_angle_offset_known = false;
 
 PID_struct pid = {0.1, 0.1, 0, 0, 0, -50, 50};
 Servo s1;
@@ -85,7 +89,7 @@ double convert_to_motor_angle(double beam_angle) {
   double c_dist = sqrt(pow(L_A1, 2) + pow(L_A2, 2) - 2*L_A1*L_A2*cos(C_angle)); // dist from motor axis to small hinge
   double phi_1 = acos((-pow(L_A2, 2) + pow(L_A1, 2) + pow(c_dist, 2)) / (2*L_A1*c_dist)); // upper triangle angle
   double phi_2 = acos((-pow(L_B2, 2) + pow(L_B1, 2) + pow(c_dist, 2)) / (2*L_B1*c_dist)); // lower triangle angle
-  return (phi_1 - (M_PI - PHI_0) + phi_2); // measured from vertical
+  return PHI_0 + phi_1 + phi_2 - M_PI/2.0; // measured from vertical
 }
 
 //////////
@@ -104,20 +108,27 @@ void loop() {
   cur_pos = convert_sensor_value(analogRead(sensor_pin));
   Serial.println(cur_pos);
 
+  // 5 second moving average of velocity
+  circbuff.addSample((cur_pos - prev_pos) * LOOP_RATE_HZ);
+  double vel_avg = circbuff.getAverage();
+
+  // when ball has reached an equilibrium point and stopped moving for 5 seconds, update beam_angle_offset
+  if (!beam_angle_offset_known && !isnan(vel_avg) && vel_avg < 0.01) {
+    beam_angle_offset = beam_angle;
+    beam_angle_offset_known = true;
+  }
+
   // PID on ball position
   double des_acceleration = calc_PID(pid, cur_pos, set_point, 1.0 / LOOP_RATE_HZ);
 
   // convert to beam angle using input linearization - not able to sense or calculate beam_angular_velocity well, so not using it at all
   beam_angular_velocity = 0.0;
-  double beam_angle = input_linearization(des_acceleration, cur_pos, beam_angular_velocity);
+  double beam_angle = input_linearization(des_acceleration, cur_pos, beam_angular_velocity) + beam_angle_offset;
   beam_angle = min(max(beam_angle, -MAX_BEAM_ANGLE), MAX_BEAM_ANGLE);
 
   // convert to motor angle using law of cosines relationship
   double motor_angle = convert_to_motor_angle(beam_angle);
   motor_angle *= 180 / M_PI;
-
-  // TODO: find this relationship using calibration or an acceleromter?
-  motor_angle = 90 + motor_angle;
 
   // clamp and write to servo
   s1.write(min(max(motor_angle, MIN_MOTOR_ANGLE), MAX_MOTOR_ANGLE));
